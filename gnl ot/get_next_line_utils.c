aa005921377ef5ad7684052c5cc63ce53196f5cb
eaga-agu <eagaaguirre@gmail.com>
2025-05-08 11:40:11 +0200

    subidos

---
+++ gnl ot/get_next_line_utils.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eaga-agu <eaga-agu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/25 12:54:06 by otboumeh          #+#    #+#             */
+/*   Updated: 2025/05/08 11:35:39 by eaga-agu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/* This function concatenates two strings (deposit and buffer) and returns 
+the resulting string. If deposit is NULL, it initializes it as an empty 
+string. It dynamically allocates memory for the concatenated string and 
+frees the old deposit string. */
+
+char *ft_strjoin(char *s1, const char *s2)
+{
+    if (!s2)
+        return (NULL);
+    if (!s1)
+    {
+        s1 = malloc(1);
+        if (!s1)
+            return (NULL);
+        s1[0] = '\0';
+    }
+    char *result = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+    if (!result)
+        return (NULL);
+    int i = 0, j = 0;
+    while (s1[i])
+    {
+        result[i] = s1[i];
+        i++;
+    }
+    while (s2[j])
+        result[i++] = s2[j++];
+    result[i] = '\0';
+    free(s1);
+    return result;
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	ch;
+
+	if (!s)
+		return (NULL);
+	ch = (char)c;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == ch)
+		{
+			return ((char *)&s[i]);
+		}
+		i++;
+	}
+	if (s[i] == ch)
+		return ((char *)&s[i]);
+	return (NULL);
+}
